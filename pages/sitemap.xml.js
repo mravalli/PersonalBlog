da2ca09b8d6ea4606bc27923db9c4587b580d45b
Mario Ravalli <mario@raval.li>
2021-11-15 13:11:26 +0100

    Removed not existant static page from sitemap.xml

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -10,57 +10,51 @@ export const getServerSideProps = ({ res }) => {
     production: "https://mario.raval.li",
   }[process.env.NODE_ENV];
 
-  const staticPages = fs
-    .readdirSync({
-      development: 'pages',
-      production: './',
-    }[process.env.NODE_ENV])
-    .filter((staticPage) => {
-      return ![
-        "_app.js",
-        "_document.js",
-        "_error.js",
-        "sitemap.xml.js",
-        "package.json",
-        "node_modules",
-        ".next",
-        "__nex_launcher.js",
-        "__vc_bridge.js",
-      ].includes(staticPage);
-    })
-    .map((staticPagePath) => {
-      return `${baseUrl}/${staticPagePath}`;
-    });
+  // const staticPages = fs
+  //   .readdirSync({
+  //     development: 'pages',
+  //     production: './',
+  //   }[process.env.NODE_ENV])
+  //   .filter((staticPage) => {
+  //     return ![
+  //       "_app.js",
+  //       "_document.js",
+  //       "_error.js",
+  //       "sitemap.xml.js",
+  //       "package.json",
+  //       "node_modules",
+  //       ".next",
+  //       "__nex_launcher.js",
+  //       "__vc_bridge.js",
+  //     ].includes(staticPage);
+  //   })
+  //   .map((staticPagePath) => {
+  //     return `${baseUrl}/${staticPagePath}`;
+  //   });
 
   const posts = getSortedPostsData()
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${staticPages
-        .map((url) => {
+      <url>
+        <loc>${baseUrl}</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>1.0</priority>
+      </url>
+      ${posts
+        .map(({id, date}) => {
           return `
             <url>
-              <loc>${url}</loc>
-              <lastmod>${new Date().toISOString()}</lastmod>
-              <changefreq>monthly</changefreq>
+              <loc>${baseUrl}/posts/${id}</loc>
+              <lastmod>${new Date(date).toISOString()}</lastmod>
+              <changefreq>yearly</changefreq>
               <priority>1.0</priority>
             </url>
-          `;
+          `
         })
-        .join("")}
-       ${posts
-         .map(({id, date}) => {
-            return `
-              <url>
-                <loc>${baseUrl}/posts/${id}</loc>
-                <lastmod>${new Date(date).toISOString()}</lastmod>
-                <changefreq>yearly</changefreq>
-                <priority>1.0</priority>
-              </url>
-            `
-          })
-         .join("")
-       }
+        .join("")
+      }
     </urlset>
   `;
 
